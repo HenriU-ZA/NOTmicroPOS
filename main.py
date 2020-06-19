1e629ba82cfd8db50cb0cc8b3668ddffcb3c7637
HenriU <rhino888@gmail.com>
2020-06-19 23:28:52 +0200

    Setting layout

--- main.py
+++ main.py
@@ -16,6 +16,10 @@ def load_user():
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 
 @app.route('/logout')
 def logout():

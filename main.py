79928af0a5b34e89ecea0664aef4f0c8401c04e3
HenriU <rhino888@gmail.com>
2020-06-20 21:35:38 +0200

    Finished login and loaded page data from db

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, session, redirect, request, url_for, g
 from pyscripts.user import User
 from pyscripts.database import Database
+from pyscripts.functiondata import FunctionData
 
 app = Flask(__name__)
 app.secret_key ='1234jhghgahugsjn'
@@ -17,7 +18,7 @@ def load_user():
 @app.route('/')
 def index():
     if g.user:
-        return render_template('index.html')
+        return render_template('index.html', pg_data=FunctionData.format_page_data('home', g.user.name))
     return redirect(url_for('login'))
 
 
@@ -28,7 +29,7 @@ def login():
         if User.login_user(request.form['user_code'], request.form['password']):
             session['user_code'] = request.form['user_code']
             return redirect(url_for('index'))
-        return redirect(url_for('error'))
+        return redirect(url_for('error', msg="Your login attempt failed!"))
     return render_template('login.html')
 
 
@@ -45,7 +46,7 @@ def test():
 
 @app.route('/error')
 def error():
-    return render_template('error_page.html')
+    return render_template('error_page.html', pg_data=FunctionData.format_page_data('error', '-----', 'There was an error in your last request.'))
 
 
 app.run(port=4995, debug=True)
